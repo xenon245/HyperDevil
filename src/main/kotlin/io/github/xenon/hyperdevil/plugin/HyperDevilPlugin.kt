6a72d0b6e33c7fe97ff32ae277c7fca6058c4884
BuildTools <unconfigured@null.spigotmc.org>
2021-08-23 22:04:58 +0900

    Devil Breath 추가

--- src/main/kotlin/io/github/xenon/hyperdevil/plugin/HyperDevilPlugin.kt
+++ src/main/kotlin/io/github/xenon/hyperdevil/plugin/HyperDevilPlugin.kt
@@ -1,6 +1,7 @@
 package io.github.xenon.hyperdevil.plugin
 
 import io.github.monun.kommand.kommand
+import io.github.monun.tap.event.EntityEventManager
 import io.github.monun.tap.fake.*
 import io.github.monun.tap.math.normalizeAndLength
 import io.github.monun.tap.util.isDamageable
@@ -13,9 +14,11 @@ import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.player.PlayerToggleSneakEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.util.BoundingBox
@@ -25,10 +28,17 @@ import kotlin.random.Random.Default.nextFloat
 class HyperDevilPlugin : JavaPlugin() {
     lateinit var fakeEntityServer: FakeEntityServer
     lateinit var fakeProjectileManager: FakeProjectileManager
+    lateinit var entityEventManager: EntityEventManager
+
+    var toggled = false
+
+    var devil = Bukkit.getPlayerExact("xenon2452")!!
 
     override fun onEnable() {
         fakeProjectileManager = FakeProjectileManager()
         fakeEntityServer = FakeEntityServer.create(this)
+        entityEventManager = EntityEventManager(this)
+
         setupKomamnds(this, server)
     }
 
@@ -38,7 +48,7 @@ class HyperDevilPlugin : JavaPlugin() {
                 executes {
                     feedback(text().color(TextColor.color(194, 0, 0)).content("HyperDevil").build())
                     server.scheduler.runTaskTimer(plugin, HyperDevilScheduler(), 0L, 1L)
-                    server.pluginManager.registerEvents(HyperDevilListener(), plugin)
+                    entityEventManager.registerEvents(devil, HyperDevilListener())
                     Bukkit.getOnlinePlayers().forEach {
                         fakeEntityServer.addPlayer(it)
                     }
@@ -51,6 +61,8 @@ class HyperDevilPlugin : JavaPlugin() {
         @EventHandler
         fun onJoin(event: PlayerJoinEvent) {
             fakeEntityServer.addPlayer(event.player)
+            event.player.isVisualFire = false
+            event.player.fireTicks = 0
         }
         @EventHandler
         fun onQuit(event: PlayerQuitEvent) {
@@ -59,6 +71,7 @@ class HyperDevilPlugin : JavaPlugin() {
         @EventHandler
         fun onInteract(event: PlayerInteractEvent) {
             if (event.item?.type == Material.BLAZE_ROD) {
+                if(event.action != Action.RIGHT_CLICK_AIR && event.action == Action.RIGHT_CLICK_BLOCK) return
                 val fakeEntity = fakeEntityServer.spawnEntity(event.player.location, ArmorStand::class.java).apply {
                     updateMetadata<ArmorStand> {
                         isInvisible = true
@@ -72,6 +85,10 @@ class HyperDevilPlugin : JavaPlugin() {
                 projectile.velocity = event.player.location.direction.multiply(1.5)
                 fakeProjectileManager.launch(event.player.location, projectile)
             }
+        }
+        @EventHandler
+        fun onPlayerSneak(event: PlayerToggleSneakEvent) {
+            toggled = event.player.itemInHand.type == Material.FLINT_AND_STEEL && event.isSneaking
         }
     }
 
@@ -148,10 +165,62 @@ class HyperDevilPlugin : JavaPlugin() {
         }
     }
 
+    private fun playDevilBreath() {
+        val player = devil
+        val breathLoc = player.eyeLocation.clone().apply {
+            y += 0.5
+        }
+        val vector = player.eyeLocation.direction.multiply(0.15)
+        val dx = vector.x
+        val dy = vector.y
+        val dz = vector.z
+        val wiggle = 0.4
+        for (i in 0 until 10) {
+            player.world.spawnParticle(Particle.FLAME,
+                breathLoc.clone().add(vector),
+                0,
+                dx + nextFloat() * wiggle - wiggle / 3.0,
+                dy + nextFloat() * wiggle - wiggle / 3.0,
+                dz + nextFloat() * wiggle - wiggle / 3.0,
+                0.5, null, true)
+        }
+
+        val filter = Predicate<LivingEntity> { entity ->
+            when(entity) {
+                player -> false
+                is Player -> entity.gameMode.isDamageable
+                else -> true
+            }
+        }
+
+        player.eyeLocation.clone().add(vector).getNearbyLivingEntities(dx.times(4.0), dy.times(4.0), dz.times(4.0), filter).forEach { entity ->
+            Bukkit.getScheduler().runTaskTimer(this@HyperDevilPlugin, BurnEntityScheduler(entity), 0L, 1L)
+        }
+    }
+
     inner class HyperDevilScheduler : Runnable {
         override fun run() {
             fakeEntityServer.update()
             fakeProjectileManager.update()
+            if (toggled) playDevilBreath()
+            if (devil.itemInHand.type != Material.FLINT_AND_STEEL) toggled = false
+        }
+    }
+
+    inner class BurnEntityScheduler(val entity: LivingEntity): Runnable {
+        private var ticks = 0
+        override fun run() {
+            ticks++
+            when (ticks) {
+                2 -> {
+                    entity.isVisualFire = true
+                    entity.fireTicks = 100
+                }
+                100 -> {
+                    entity.isVisualFire = false
+                    entity.fireTicks = -1
+                }
+            }
         }
     }
 }
